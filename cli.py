7990dcbf843756316cb8f099545d39b51557e8d9
Will Maier <willmaier@ml1.net>
2009-01-02 11:37:32 -0600

    Support finding and running a callable.

--- cli.py
+++ cli.py
@@ -1,7 +1,73 @@
+import sys
+
+from inspect import getargs
+
+class Error(Exception):
+    pass
+
+class MainError(Error):
+    pass
+
 class App(object):
+    """A command-line application.
+
+    Command-line applications (CLI apps) perform a task based in
+    part on options and arguments passed on the command line. The
+    App object makes it easy to develop simple CLI apps.
+
+    Instantiated CLI apps parse options, arguments and (optionally)
+    other configuration information and pass them to a single
+    callable. This callable must support the App interface, which
+    means its signature should be something like the following:
+
+        def main(opts, args, app):
+
+    Whether or not the callable actually _uses_ any of this
+    information is optional, though it must accept them.
+    """
+
+    def __init__(self, main=None, exit_after_main=True):
+        self.main = main
+        self.exit_after_main = exit_after_main
+
+    def find_main(self, main=None):
+        """Find a suitable main() callable.
+
+        If the supplied 'main' argument is not a callable, search
+        globals() for a suitable callable. If no callable is found
+        or the chosen callable doesn't support the App interface
+        (see cli.App.__doc__ for more information), raise MainError.
+        """
+        if not callable(main):
+            main = globals().get('main', None)
+
+        if not callable(main):
+            raise MainError("Could not find main()")
+
+        args, varargs, varkw = getargs(main.func_code)
+
+        if len(args) != 3 or not (varargs or varkw)):
+            raise MainError("main() must take three arguments or varargs/varkw")
+
+        return main
 
     def run(self):
-        pass
+        """Run the application's callable.
+
+        If the callable hasn't already been determined, discover it
+        and pass it the resolved options, arguments and Application
+        object. If the App.exit_after_main is True, call sys.exit()
+        with the return value of the application's callable.
+        Otherwise, return the result.
+        """
+        main = self.find_main(self.main)
+
+        returned = main(self.opts, self.args, self)
+
+        if self.exit_after_main:
+            sys.exit(returned)
+        else:
+            return returned
 
 def main(opts, args, app=None):
     print 'cli.App test!'
