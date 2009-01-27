b89d399af73fccdbf3b3c0b4d89ed7b7ba3e556d
Will Maier <willmaier@ml1.net>
2009-01-27 17:20:38 -0600

    s/run/pre_run/ in LoggingApp.

--- cli.py
+++ cli.py
@@ -375,14 +375,9 @@ class LoggingApp(CommandLineApp):
         self.log.setLevel(self.log.default_level)
         self.log.addHandler(handler)
 
-    def run(self):
-        """Run the app.
-
-        Before running, set the logger's verbosity level based on
-        the CLI options.
-        """
-        self.log.setLevel(opts=self.values[0])
-        super(LoggingApp, self).run()
+    def pre_run(self):
+        """Configure logging before running the app."""
+        self.log.setLevel(opts=self.params)
 
 App = LoggingApp
 
