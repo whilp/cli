56efad45cd5e1d49aec19ea2d4fded4a9bbf755a
Will Maier <willmaier@ml1.net>
2009-01-29 16:11:07 -0600

    Add missing import.

--- cli/util.py
+++ cli/util.py
@@ -15,6 +15,7 @@ ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
 OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 
 """
+from UserDict import UserDict
 
 class AttributeDict(UserDict, object):
     """A dict that maps its keys to attributes."""
