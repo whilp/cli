f52d17cd94476bbf5c4eb9b4a7ec3539b00c2454
Will Maier <willmaier@ml1.net>
2009-01-30 11:28:44 -0600

    Add a test command.

--- setup.py
+++ setup.py
@@ -1,7 +1,22 @@
 import os
-from distutils.core import setup
+from distutils.core import Command, setup
 
 import cli
+
+class TestCommand(Command):
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        from cli import App
+        from cli.test import test
+        app = App(test, argv=[os.getcwd()])
+        app.run()
 
 docs = [os.path.join('docs', x) for x in os.listdir('docs')]
 examples = docs = [os.path.join('examples', x) for x in os.listdir('examples')]
@@ -17,4 +32,5 @@ setup(name="CLI",
         data_files=[
             ('share/doc/py-cli/', docs),
             ('share/examples/py-cli/', examples)],
-        py_modules=['cli'])
+        py_modules=['cli'],
+        cmdclass = {"test": TestCommand})
