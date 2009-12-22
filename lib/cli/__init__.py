ff78b4e42fb0ea892b06b9e3ddcebf68231ebfec
Will Maier <willmaier@ml1.net>
2009-12-22 14:57:29 -0600

    Bump version.

--- lib/cli/__init__.py
+++ lib/cli/__init__.py
@@ -10,7 +10,7 @@ consistent application interface.
 """
 
 __project__ = "pyCLI"
-__version__ = "0.3.3"
+__version__ = "0.3.4"
 __package__ = "cli"
 __description__ = "Simple, Object-Oriented approach to Python CLI apps"
 __author__ = "Will Maier"
