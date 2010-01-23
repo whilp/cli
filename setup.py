523e106910672f791bbf353433cc7ae92bfda69c
Will Maier <willmaier@ml1.net>
2010-01-22 22:57:01 -0600

    Initialize.

---
+++ setup.py
@@ -0,0 +1,37 @@
+try:
+    import setuptools
+except ImportError:
+    from distribute_setup import use_setuptools
+    use_setuptools()
+from setuptools import setup, find_packages
+
+import sys, os
+
+libdir = "lib"
+
+sys.path.insert(0, libdir)
+
+import project as pkg
+
+setup_options = {
+    "name": pkg.__project__,
+    "version": pkg.__version__,
+    "description": pkg.__description__,
+    "long_description": pkg.__doc__,
+    "classifiers": pkg.__classifiers__,
+    "keywords": pkg.__keywords__,
+    "author": pkg.__author__,
+    "author_email": pkg.__author_email__,
+    "url": pkg.__url__,
+    "packages": find_packages(libdir),
+    "package_dir": {"": libdir},
+    "include_package_data": True,
+    "zip_safe": False,
+    "install_requires": pkg.__requires__,
+    "entry_points": """
+        # -*- Entry points: -*-
+    """,
+    "test_suite": "tests",
+}
+
+setup(**setup_options)
