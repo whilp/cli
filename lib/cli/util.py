697881ea4a8dd1a40622887ee4793cff7b00b9e0
Will Maier <willmaier@ml1.net>
2010-02-01 14:34:34 -0600

    Kill old util stuff.

--- lib/cli/util.py
+++ lib/cli/util.py
@@ -15,54 +15,6 @@ ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
 OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 
 """
-from UserDict import UserDict
-
-Nothing = object()
-
-class AttributeDict(UserDict, object):
-    """A dict that maps its keys to attributes."""
-
-    def __getattribute__(self, attr):
-        """Fetch an attribute.
-
-        If the instance has the requested attribute, return it. If
-        not, look for the attribute in the .data dict. If the
-        requested attribute can't be found in either location, raise
-        AttributeError.
-        """
-        try:
-            value = super(AttributeDict, self).__getattribute__(attr)
-        except AttributeError:
-            data = super(AttributeDict, self).__getattribute__("data")
-            value = data.get(attr, Nothing)
-            if value is Nothing:
-                raise
-
-        return value
-
-def plural(number):
-    """Return 's' if number is != 1."""
-    return number != 1 and 's' or ''
-
-def Boolean(thing):
-    """Decide if 'thing' is True or False.
-
-    If thing is a string, convert it to lower case and, if it starts
-    with 'y' or 't' (as in 'yes' or 'true'), return True. If it
-    starts with some other character, return False. Otherwise,
-    return True if 'thing' is True (in the Pythonic sense).
-    """
-    if callable(getattr(thing, 'lower', None)):
-        thing = thing.lower()
-        if thing[0] in 'yt':
-            return True
-        else:
-            return False
-    elif thing:
-        return True
-    else:
-        return False
-
 class update_wrapper(object):
     assignments = ('__module__', '__name__', '__doc__')
     updates = ('__dict__',)
@@ -110,4 +62,3 @@ def fmtsec(seconds):
 
     format += " %ss"
     return format % (seconds, prefix)
-
