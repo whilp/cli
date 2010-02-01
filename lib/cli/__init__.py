de2ce9b6b6e84160091022aa8e5b37ab4e9a7ea3
Will Maier <willmaier@ml1.net>
2010-02-01 14:03:19 -0600

    Catch another copyright bump.

--- lib/cli/__init__.py
+++ lib/cli/__init__.py
@@ -33,7 +33,7 @@ __requires__ = []
 
 # The following is modeled after the ISC license.
 __copyright__ = """\
-2009 Will Maier <will@m.aier.us>
+2009-2010 Will Maier <will@m.aier.us>
 
 Permission to use, copy, modify, and distribute this software for any
 purpose with or without fee is hereby granted, provided that the above
