7fa0e900add0e77602d8ebf89abfda7bfaf543ef
Will Maier <willmaier@ml1.net>
2010-02-11 13:58:12 -0600

    Sync with base changes.

--- lib/cli/ext.py
+++ lib/cli/ext.py
@@ -1,13 +1,14 @@
-import _ext
-
 """Includes:
     argparse - 2010.02.01
         LICENSE:    Apache License 2.0
         URL:        http://argparse.googlecode.com/svn/tags/r101/argparse.py
 """
+import os
 
+# Add included module names to __all__.
 __all__ = ["argparse"]
-ext = "cli._ext"
+project = os.path.basename(os.path.dirname(__file__))
+ext = project + "._ext"
 
 name, module = None, None
 for name in __all__:
@@ -17,4 +18,4 @@ for name in __all__:
         module = __import__('.'.join((ext, name)), fromlist=[ext])
     locals()[name] = module
 
-del(ext, module, name)
+del(ext, module, name, project)
