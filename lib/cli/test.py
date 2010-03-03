7e118f6d3968281fe2214c32e83e634e243cb46a
Will Maier <willmaier@ml1.net>
2010-03-03 16:56:07 -0600

    Migrate FunctionalTest to cli.test.
    While here, add docstrings.

--- lib/cli/test.py
+++ lib/cli/test.py
@@ -22,12 +22,21 @@ OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 
 """
 
+import os
+
+from shutil import rmtree
+from tempfile import mkdtemp
+
 try:
     import unittest2 as unittest
 except ImportError:
     import unittest
 
-__all__ = ["AppTest"]
+from scripttest import TestFileEnvironment
+
+from cli.util import trim
+
+__all__ = ["AppTest", "FunctionalTest"]
 
 class AppTest(unittest.TestCase):
     """An application test, based on :class:`unittest.TestCase`.
@@ -63,3 +72,73 @@ class AppTest(unittest.TestCase):
         def app(app):
             pass
         self.app = app
+
+class FunctionalTest(unittest.TestCase):
+    """A functional test, also based on :class:`unittest.TestCase`.
+
+    Functional tests monitor an application's 'macro' behavior, making
+    it easy to spot regressions. They can also be simpler to write and
+    maintain as they don't rely on any application internals.
+
+    The :class:`FunctionalTest` will look for scripts to run under
+    :attr:`scriptdir`. It uses :class:`scripttest.TestFileEnvironment`
+    to provide temporary working areas for the scripts; these scratch
+    areas will be created under :attr:`testdir` (and are created and
+    removed before and after each test is run).
+    """
+    testdir = None
+    scriptdir = None
+
+    def setUp(self):
+        """Prepare for the functional test.
+
+        :meth:`setUp` creates the test's working directory. If
+        the unittest2 package is present, it also makes sure that
+        differences in the test's standard err and output are presented
+        using :class:`unittest2.TestCase.assertMultiLineEqual`. Finally,
+        :meth:`setUp` instantiates the
+        :class:`scripttest.TestFileEnvironment` and stores it at
+        :attr:`env`.
+        """
+        self._testdir = self.testdir
+        if self._testdir is None:
+            self._testdir = mkdtemp(prefix="functests-")
+        if not os.path.isdir(self._testdir):
+            os.mkdir(self._testdir)
+        self.env = TestFileEnvironment(os.path.join(self._testdir, "scripttest"))
+
+        addTypeEqualityFunc = getattr(self, "addTypeEqualityFunc", None)
+        if callable(addTypeEqualityFunc):
+            addTypeEqualityFunc(str, "assertMultiLineEqual")
+
+    def tearDown(self):
+        """Clean up after the test.
+
+        :meth:`tearDown` removes the temporary working directory created
+        during :meth:`setUp`.
+        """
+        rmtree(self._testdir)
+
+    def run_script(self, script, *args, **kwargs):
+        """Run a test script.
+
+        *script* is prepended with the path to :attr:`scriptdir` before
+        it, *args* and *kwargs* are passed to :attr:`env`.
+        """
+        script = os.path.join(self.scriptdir, script)
+        return self.env.run(script, *args, **kwargs)
+
+    def assertScriptDoes(self, result, stdout='', stderr='', returncode=0, trim_output=True):
+        """Fail if the result object's stdout, stderr and returncode are unexpected.
+
+        *result* is usually a :class:`scripttest.ProcResult` with
+        stdout, stderr and returncode attributes.
+        """
+        if trim_output:
+            stdout, stderr = trim(stdout), trim(stderr)
+        self.assertEqual(result.returncode, returncode,
+            "expected returncode %d, got %d" % (result.returncode, returncode))
+        self.assertEqual(result.stdout, stdout,
+            "unexpected output on stdout")
+        self.assertEqual(result.stderr, stderr,
+            "unexpected output on stderr")
--- tests/functional/__init__.py
+++ tests/functional/__init__.py
@@ -1,43 +1,5 @@
 import os
 
-from shutil import rmtree
-from tempfile import mkdtemp
+from cli.test import FunctionalTest
 
-from scripttest import TestFileEnvironment
-
-from cli.util import trim
-
-from tests import BaseTest
-
-class FunctionalTest(BaseTest):
-    testdir = None
-    scriptdir = os.path.join(os.path.dirname(__file__), "scripts")
-
-    def setUp(self):
-        self._testdir = self.testdir
-        if self._testdir is None:
-            self._testdir = mkdtemp(prefix="functests-")
-        if not os.path.isdir(self._testdir):
-            os.mkdir(self._testdir)
-        self.env = TestFileEnvironment(os.path.join(self._testdir, "scripttest"))
-
-        addTypeEqualityFunc = getattr(self, "addTypeEqualityFunc", None)
-        if callable(addTypeEqualityFunc):
-            addTypeEqualityFunc(str, "assertMultiLineEqual")
-
-    def tearDown(self):
-        rmtree(self._testdir)
-
-    def run_script(self, script, *args, **kwargs):
-        script = os.path.join(self.scriptdir, script)
-        return self.env.run(script, *args, **kwargs)
-
-    def assertScriptDoes(self, result, stdout='', stderr='', returncode=0, trim_output=True):
-        if trim_output:
-            stdout, stderr = trim(stdout), trim(stderr)
-        self.assertEqual(result.returncode, returncode,
-            "expected returncode %d, got %d" % (result.returncode, returncode))
-        self.assertEqual(result.stdout, stdout,
-            "unexpected output on stdout")
-        self.assertEqual(result.stderr, stderr,
-            "unexpected output on stderr")
+FunctionalTest.scriptdir = os.path.join(os.path.dirname(__file__), "scripts")
