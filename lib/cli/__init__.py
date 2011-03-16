dccbfb650036a992136c0583935925d7e9a2b948
Will Maier <willmaier@ml1.net>
2011-03-16 12:16:41 -0500

    Bump version.
    --HG--
    branch : stable

--- lib/cli/__init__.py
+++ lib/cli/__init__.py
@@ -16,7 +16,7 @@ interface.
 # pragma: no cover
 
 __project__ = "pyCLI"
-__version__ = "2.0.1"
+__version__ = "2.0.2"
 __package__ = "cli"
 __description__ = "Simple, object-oriented approach to Python CLI apps"
 __author__ = "Will Maier"
